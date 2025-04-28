83f8f56d57f62fb5e1a92fc7f2420ce045aa2db5
TheRealCringengineer <cringengineer_was_taken@proton.me>
2025-04-28 23:45:06 +0300

    Change : binds for multicursor

--- lua/plugins/multicursor.lua
+++ lua/plugins/multicursor.lua
@@ -1,8 +1,12 @@
 return {
   {
     "mg979/vim-visual-multi",
-    config = function()
+    init = function()
+      vim.g.VM_default_mappings = 0
+      vim.g.VM_add_cursor_at_pos_no_mappings = 1
 
+      vim.keymap.set("n", "<C-Down>", "<Plug>(VM-Add-Cursor-Down)", { desc = "Add cursor down" })
+      vim.keymap.set("n", "<C-Up>", "<Plug>(VM-Add-Cursor-Up)", { desc = "Add cursor up" })
     end,
   }
 }
