60314a3ff7d260ceef10b56b49c4bc7125b1e2c9
TheRealCringengineer <cringengineer_was_taken@proton.me>
2025-10-20 03:04:28 +0300

    Rework : diagnostic line

--- lua/vim-params.lua
+++ lua/vim-params.lua
@@ -16,3 +16,16 @@ vim.wo.number = true -- Or relativenumber
 
 vim.keymap.set("n", "<leader>q", "<cmd>bd<CR>", { desc = "Close current buffer" })
 vim.keymap.set("", "<esc>", "<esc><cmd>noh<CR>", { noremap = true })
+vim.lsp.set_log_level("ERROR")
+
+vim.keymap.set('n', '<leader>k', function()
+  vim.diagnostic.config({ virtual_lines = { current_line = true }, virtual_text = false })
+
+  vim.api.nvim_create_autocmd('CursorMoved', {
+    group = vim.api.nvim_create_augroup('line-diagnostics', { clear = true }),
+    callback = function()
+      vim.diagnostic.config({ virtual_lines = false, virtual_text = true })
+      return true
+    end,
+  })
+end, { desc = "Diagnostic virtual lines" })
